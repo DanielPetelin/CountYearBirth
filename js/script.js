9d24a5218f25a381856a6edad994cc8f8005d528
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-10 15:18:21 +0300

    nice

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var numYear = 2019;
+var how = prompt('введите ваше имя');
+var years = prompt('сколоько вам лет');
+var resultBirth = numYear - years;
+
+alert(how + ', ваш год рождения ' + resultBirth);
